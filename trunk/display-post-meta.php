9a52147214e3f7acb528d4c0d0611c3c5123e00c
BrashRebel <BrashRebel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-19 21:05:13 +0000

    Updating the trunk to 1.3
    git-svn-id: https://plugins.svn.wordpress.org/display-post-meta@935388 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/display-post-meta.php
+++ trunk/display-post-meta.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Display Post Meta
 Description: This plugin allows you to display the meta data for a post. Just click the DPM link in the toolbar to show meta info.
-Version: 1.2.0
+Version: 1.3
 Author: Kyle Maurer
 Author URI: http://realbigmarketing.com/staff/kyle
 */
@@ -10,56 +10,82 @@ Author URI: http://realbigmarketing.com/staff/kyle
 /*
 Credit for taxonomies part goes here http://stackoverflow.com/questions/14956624/show-all-taxonomies-for-a-specific-post-type
 */
-function get_post_meta_all($post_id){
-    global $wpdb;
-    $data   =   array();
-    $wpdb->query("
-        SELECT `meta_key`, `meta_value`
-        FROM $wpdb->postmeta
-        WHERE `post_id` = $post_id
-    ");
-    foreach($wpdb->last_result as $k => $v){
-        $data[$v->meta_key] =   $v->meta_value;
-    };
-    return $data;
-}
-// Register stylesheet
-function dpm_register_style() {
-  $show_meta = isset($_GET['show_meta']) ? true : false;
-  wp_register_style( 'DPMstyle', plugins_url('style.css', __FILE__) );
-    if ($show_meta === true) {
-      wp_enqueue_style( 'DPMstyle' );
-    }
-}
-add_action('wp_enqueue_scripts', 'dpm_register_style');
-//Create shortcode
-function dpm_display( ){
-if ( is_user_logged_in() ) {
-$meta = get_post_meta_all(get_the_ID());
-   echo '<div class="show-meta"><span class="meta-tab">All post meta data</span><p class="meta-data">';
-   print("<pre>".print_r($meta,true)."</pre>");
-//Taxonomies
-  $id = get_the_ID();
-  foreach ( get_object_taxonomies( 'post' ) as $taxonomy ) {
-    $terms_list = get_the_term_list( $id, $taxonomy, '<ul class="entry-taxonomies"><ul class="tax-terms"><li>', ''.__( '', '' ).'</li><li>','</li></ul></div>' );
-    if ( $terms_list ) {?>                  
-     <div class="tax-tab"><span class="tax-taxonomy"><?php echo $taxonomy; ?></span><?php echo $terms_list; ?><?php
-	}
-  }?>
 
-  </ul></p></div></div>
-<?php
-}
-}
+class DisplayPostMeta {
 
-add_shortcode( 'show_meta', 'display_meta' );
-function dpm_activate() {
-  $show_meta = isset($_GET['show_meta']) ? true : false;
-  if ($show_meta === true) {
-    dpm_display();
+  public function __construct() {
+      add_action( 'wp_footer', array( $this, 'activate' ) );
+      add_action( 'wp_enqueue_scripts', array( $this, 'register_style' ) );
   }
-}
-add_action('wp', 'dpm_activate');
 
+  // Get all the custom field data
+  function get_post_meta_all( $post_id ) {
+      global $wpdb;
+      $data   =   array();
+      $wpdb->query("
+          SELECT `meta_key`, `meta_value`
+          FROM $wpdb->postmeta
+          WHERE `post_id` = $post_id
+      ");
+      foreach($wpdb->last_result as $k => $v){
+          $data[$v->meta_key] =   $v->meta_value;
+      };
+      return $data;
+  }
+
+  // Register stylesheet
+  function register_style() {
+    $show_meta = isset( $_GET['show_meta'] ) ? true : false;
+    wp_register_style( 'DPMstyle', plugins_url('style.css', __FILE__) );
+      if ( $show_meta === true ) {
+        wp_enqueue_style( 'DPMstyle' );
+      }
+  }
+
+  // Display custom field data
+  public function custom_fields() {
+    $id = get_the_ID();
+    $meta = $this->get_post_meta_all( $id );
+    echo '<span class="meta-tab">Custom Fields</span>';
+    if ( $meta ) {
+      echo '<ul>';
+      foreach ( $meta as $key => $value ) {
+        echo '<li><strong>'.$key.'</strong>: ';
+        echo $value.'</li>';
+      }
+      echo '</ul>';
+    } else { echo 'This post has no custom field data.'; }
+  }
+
+  // Get and display taxonomies
+  public function taxonomies() {
+    $post_type = get_post_type();
+    $id = get_the_ID();
+      foreach ( get_object_taxonomies( $post_type ) as $taxonomy ) {
+        $terms_list = get_the_term_list( $id, $taxonomy, '<ul class="entry-taxonomies"><ul class="tax-terms"><li>', ''.__( '', '' ).'</li><li>','</li></ul></div>' );
+        if ( $terms_list ) { ?>                  
+         <div class="tax-tab">
+          <span class="tax-taxonomy">
+            <?php echo $taxonomy; ?>
+          </span>
+          <?php echo $terms_list;
+        }
+      } ?>
+      </ul></div>
+    <?php
+  }
+
+  // Actually display the stuff
+  public function activate() {
+    $show_meta = isset( $_GET['show_meta'] ) ? true : false;
+    if ( $show_meta === true && is_user_logged_in() ) {
+      echo '<div class="show-meta">';
+      $this->custom_fields();
+      $this->taxonomies();
+      echo '</div>';
+    }
+  }
+
+}
+$dpm = new DisplayPostMeta;
 include('admin-bar.php');
-?>
